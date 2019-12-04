ea1a6e166ff09eca8a6ae4a74df89bb6378efb93
Your Name <you@example.com>
2019-12-04 10:28:36 +0900

    :+1: seed作成

---
+++ db/seeds/sql/insert_attribute.go
@@ -0,0 +1,24 @@
+package sql
+
+// InsertMockAttribute insert attribute
+var InsertMockAttribute = `
+INSERT INTO attributes(id, name) VALUES
+('bee031a9-3574-4f67-84d5-7efd2845391c','ノーマル'),
+('2dd4ea36-d42f-46ff-9200-f0864278813b','ほのお'),
+('172adcac-fa39-46b8-b3b7-7ccc6d261b9e','みず'),
+('92b68344-6949-494b-afe1-02f7d2e389cd','くさ'),
+('c0fca774-1f69-4ba9-83ed-5d8cca224f54','でんき'),
+('983abd4d-6d44-456f-b109-ab79f8708b55','こおり'),
+('5d45130a-8b3f-4f29-a2d1-5a6e5cde2613','かくとう'),
+('00dbd96b-28fc-422c-a290-210dd28039a4','どく'),
+('3a861f05-6be5-45f1-8c4f-99d7f824717b','じめん'),
+('6e6e8781-f68c-4a1a-b1ce-ee17654db0d6','ひこう'),
+('b92ae8c9-f200-4c4a-a713-75f78d823bb6','エスパー'),
+('87f8efb5-2afc-4900-b4c7-6b77a007887b','むし'),
+('55eec6f0-7963-4c80-9d4f-f72ad2d63d8a','いわ'),
+('58f1ff72-e4df-462a-937a-95db14ecffab','ゴースト'),
+('db9af348-5266-4f8d-92b6-e8adca6bfdc9','ドラゴン'),
+('2b2d4589-95df-4e46-8b6d-7f7f34f370bf','あく'),
+('132f0a26-129b-4e37-91eb-7f76dd80cad1','はがね'),
+('c88fc92d-73e0-4243-b54d-d1ff84676b47','フェアリー');
+`
