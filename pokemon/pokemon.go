89be522bf473b498afc9d68e596a3f831cf1a282
Your Name <you@example.com>
2019-12-04 09:31:14 +0900

    タイプテーブル作成

--- pokemon/pokemon.go
+++ pokemon/pokemon.go
@@ -1,9 +1,20 @@
 package pokemon
 
-import uuid "github.com/satori/go.uuid"
+import (
+	uuid "github.com/satori/go.uuid"
+	"gitlab.com/pokemon-party-meta-chart/pokemon-tool-api/attribute"
+)
 
 // Pokemon struct
 type Pokemon struct {
 	ID   uuid.UUID `json:"id"`
 	Name string    `json:"name"`
+}
+
+// Response struct
+type Response struct {
+	ID         uuid.UUID             `json:"id"`
+	Name       string                `json:"name"`
+	Attributes []attribute.Attribute `json:"types"`
+	Weak       []attribute.Attribute `json:"weaks"`
 }
